a2fd2d4f4abf1ff42e5960ba20ff6ca72b419fa3
opsb <oliver@opsb.co.uk>
2011-08-04 12:14:18 +0100

    Allow delay to be specified on tests ENV['TEST_DELAY']

--- test/model_test.rb
+++ test/model_test.rb
@@ -2,6 +2,8 @@ require 'test/unit'
 
 class GoodTest < Test::Unit::TestCase
   def test_good
+    delay = ENV["TEST_DELAY"] ? ENV["TEST_DELAY"].to_i : 120
+    sleep delay
     assert true
   end
 end
